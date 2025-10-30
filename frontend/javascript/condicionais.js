fd2c35c1441ddf665036a6d800107a15de8da539
Guilherme <guifefo@gmail.com>
2025-10-29 21:15:42 -0300

    Delete frontend directory

--- frontend/javascript/condicionais.js
+++
@@ -1,7 +0,0 @@
-// Condicionais
-if (idade >= 18) {
-    console.log(nome + ' é maior de idade.');
-} else {
-    console.log(nome + ' é menor de idade.');
-    
-}
--- frontend/javascript/constantes.js
+++
@@ -1,9 +0,0 @@
-
-// Costantes
-const idade = 28;
-console.log('Idade: ' + idade);
-
-
-// Tentativa de alterar o valor da constante (isso causará um erro)
-// idade = 30; // Descomente esta linha para ver o erro
-// console.log('Nova Idade: ' + idade);
